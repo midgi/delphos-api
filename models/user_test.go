3981c18223fbd65b3132b9ad0e4ea32f80499b88
Luan Santos <cfcluan@gmail.com>
2016-02-14 09:58:43 -0800

    Don't dot import packate being tested
    Also, updates path to models to new organization

--- models/user_test.go
+++ models/user_test.go
@@ -1,19 +1,16 @@
 package models_test
 
 import (
-	. "github.com/kirederik/delphos/models"
-
+	"github.com/migdi/delphos-api/models"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("User", func() {
 	Context("when creating users", func() {
-
 		It("must encrypt the password", func() {
-			user := NewUser("Foo", "bar@bar.com", "foobar123")
+			user := models.NewUser("Foo", "bar@bar.com", "foobar123")
 			Expect(user.GetPassword()).NotTo(Equal("foobar123"))
 		})
-
 	})
 })
