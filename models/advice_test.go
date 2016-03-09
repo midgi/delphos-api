ea93982e98f4ddd95f81c09ca4c5cfacc6fc2ee4
Raphael de Andrade <rhdeandrade@gmail.com>
2016-03-08 22:34:22 -0300

    tests Passed

--- models/advice_test.go
+++ models/advice_test.go
@@ -1,18 +1,18 @@
 package models_test
 
 import (
-	"github.com/migdi/delphos-api/models"
+	. "."
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Advice", func() {
 	Context("when creating advices", func() {
-		It("must return adivce model", func() {
-			user := models.NewUser("Foo", "bar@bar.com", "foobar123")
-			advice := models.NewAdvice("id", "Content of Advice", user)
+		It("must return advice model attrs", func() {
+			user := NewUser("Foo", "bar@bar.com", "foobar123")
+			advice := NewAdvice("id", "Content of Advice", *user)
 			Expect(advice.GetId()).To(Equal("id"))
-			Expect(advice.GetId()).To(Equal("Content"))
+			Expect(advice.GetContent()).To(Equal("Content of Advice"))
 		})
 	})
 })
